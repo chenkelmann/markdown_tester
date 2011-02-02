df613a0e3e4e1133646d0d39ad3e873936185c50
Christoph Henkelmann <christoph@henkelmann.eu>
2011-02-02 16:10:33 +0100

    bumped junit_xml_listener to version 0.2

--- project/plugins/MarkdownTesterPlugins.scala
+++ project/plugins/MarkdownTesterPlugins.scala
@@ -4,5 +4,5 @@ class MarkdownTesterPlugins(info: ProjectInfo) extends PluginDefinition(info) {
     val repo = "GH-pages repo" at "http://mpeltonen.github.com/maven/"
     lazy val idea = "com.github.mpeltonen" % "sbt-idea-plugin" % "0.1-SNAPSHOT"
     val christophsRepo = "Christoph's Repo" at "http://maven.henkelmann.eu"
-    val junitXml = "eu.henkelmann" % "junit_xml_listener" % "0.1"
+    val junitXml = "eu.henkelmann" % "junit_xml_listener" % "0.2"
 } 
